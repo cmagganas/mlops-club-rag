e38a9ca58f5cf98cdb337e9c462ec7ea3b0d7a5d
--help <mert.bozkirr@gmail.com>
2024-10-13 01:12:17 +0300

    Refactor vector store and add data cleaning script 🧼🔬🤓

---
+++ src/data_cleaning.py
@@ -0,0 +1,53 @@
+import os
+import re
+
+def clean_srt_to_txt(file_content):
+    cleaned_lines = []
+    skip_next_line = False
+    for line in file_content.splitlines():
+        if re.match(r'^\d+$', line.strip()):
+            skip_next_line = True
+            continue
+        if skip_next_line:
+            skip_next_line = False
+            continue
+        if line.strip():
+            cleaned_lines.append(line)
+    return ' '.join(cleaned_lines)
+
+
+# Get the current directory
+# print(current_dir)
+if __name__ == "__main__":
+    current_dir = os.path.join(os.getcwd(), 'out')
+    # Loop through all files in the current directory
+    for project in os.listdir(current_dir):
+        print("project: ", project)
+        project_path = os.path.join(current_dir, project, "compositions")
+        if os.path.isdir(project_path):
+            for composition in os.listdir(project_path):
+                composition_path = os.path.join(project_path, composition)
+                if os.path.isdir(composition_path):
+                    for file in os.listdir(composition_path):
+                        if file.endswith('.srt'):
+                            srt_path = os.path.join(composition_path, file)
+                            print("srt_path: ", srt_path)
+                            print("composition: ", composition)
+                            
+                            txt_path = os.path.join(composition_path, file[:-4] + '.txt')
+                            
+                        
+                            # Read the SRT file
+                            with open(srt_path, 'r', encoding='utf-8') as file:
+                                srt_content = file.read()
+                            
+                            # Clean the content
+                            cleaned_txt_content = clean_srt_to_txt(srt_content)
+                            
+                            # Write the cleaned content to a new TXT file
+                            with open(txt_path, 'w', encoding='utf-8') as file:
+                                file.write(cleaned_txt_content)
+                            
+                            print(f"Processed {srt_path} -> {txt_path}")
+
+    print("All .srt files have been processed and converted to .txt files.")
--- src/vector_store.py
+++ src/vector_store.py
@@ -6,15 +6,26 @@ from llama_index.core import StorageContext
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 from llama_index.vector_stores.pinecone import PineconeVectorStore
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class VectorStore(BaseReader):
-    def __init__(self, input_dir, recursive=True, exclude=["*.srt"], file_metadata=lambda x: {"source": x}):
+    def __init__(self, 
+        input_dir="./out", 
+        recursive=True, 
+        exclude=["*.srt"], 
+        file_metadata=lambda x: {"source": x},
+        pinecone_api_key=os.environ['PINECONE_API'], 
+        index_name=os.getenv("PINECONE_INDEX_NAME")):
+
+
         self.input_dir = input_dir
         self.recursive = recursive
         self.exclude = exclude
         self.file_metadata = file_metadata
-        self.api_key = os.environ["PINECONE_API_KEY"]
-        self.index_name = os.environ["PINECONE_INDEX_NAME"]
+        self.pinecone_api_key = pinecone_api_key
+        self.index_name = index_name
 
     def load_data(self):
         reader = SimpleDirectoryReader(
@@ -26,24 +37,39 @@ class VectorStore(BaseReader):
         return reader.load_data()
 
     def instantiate_index(self):
-        pc = Pinecone(api_key=self.api_key)
-        if self.index_name not in list_indexes():
+        pc = Pinecone(api_key=self.pinecone_api_key)
+        if self.index_name not in pc.list_indexes().names():
             
             pc.create_index(
             name=self.index_name,
             dimension=1536,
             metric="dotproduct",
-            spec=ServerlessSpec(cloud="aws", region="us-west-2"),
+            spec=ServerlessSpec(cloud="aws", region="us-east-1"),
         )
             return pc.Index(self.index_name)
         else:
             return pc.Index(self.index_name)
     
-    def upload_documents(self):
+    def populate_index(self):
         documents = self.load_data()
         index = self.instantiate_index()
-        index.upsert(documents)
+
+        vector_store = PineconeVectorStore(
+            pinecone_index=index,
+            add_sparse_vector=True,
+        )
+        storage_context = StorageContext.from_defaults(vector_store=vector_store)
+        return VectorStoreIndex.from_documents(
+            documents, storage_context=storage_context
+        )
+
+    def query_engine(self):
+        index = self.populate_index()
+        return index.as_query_engine()
 
 
-documents = VectorStore(input_dir="./out").load_data()
-print(documents[:5])
+if __name__ == "__main__":  
+    vc = VectorStore()
+    qe = vc.query_engine()
+
+    print(qe.query("What is MLOps?"))
