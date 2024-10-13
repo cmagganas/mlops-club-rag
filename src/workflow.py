20a6f4e0d883115c1df8b1807ccc88b51e50b58e
Christos Magganas <cmagganas@gmail.com>
2024-10-13 13:57:05 -0700

    python src/workflow.py works

--- src/workflow.py
+++ src/workflow.py
@@ -159,12 +159,34 @@ async def main():
     )
     markdown_content = str(response)
     
-    # Output the markdown content to the terminal
-    display(Markdown(markdown_content))
-    
     return markdown_content
 
 if __name__ == "__main__":
     import asyncio
+    from rich.console import Console
+    from rich.markdown import Markdown
+    from opentelemetry.sdk import trace as trace_sdk
+    from opentelemetry.sdk.trace.export import SimpleSpanProcessor
+    from opentelemetry.exporter.otlp.proto.http.trace_exporter import (
+        OTLPSpanExporter as HTTPSpanExporter,
+    )
+    from openinference.instrumentation.llama_index import LlamaIndexInstrumentor
+
+
+    # Add Phoenix
+    span_phoenix_processor = SimpleSpanProcessor(
+        HTTPSpanExporter(endpoint="https://app.phoenix.arize.com/v1/traces")
+    )
+    # Add them to the tracer
+    tracer_provider = trace_sdk.TracerProvider()
+    tracer_provider.add_span_processor(span_processor=span_phoenix_processor)
+
+    # Instrument the application
+    LlamaIndexInstrumentor().instrument(tracer_provider=tracer_provider)
+
     output = asyncio.run(main())
-    print(output)
+    
+    # Output the markdown content to the terminal using rich
+    console = Console()
+    md = Markdown(output)
+    console.print(md)
